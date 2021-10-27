b384e3c4a3990e615f5d5e5297039beecbf2c64f
Tay <tmitsumori96@gmail.com>
2021-10-27 17:08:37 -0500

    bank_account defined

---
+++ bankaccount.py
@@ -0,0 +1,8 @@
+from random import randint 
+bank_accounts[]
+
+def BankAccount():
+    def __init__:(self, full_name, account_number, balance =0):
+    self.full_name = full_name
+    self.accounts_number = account_number 
+    self.balance = balance
