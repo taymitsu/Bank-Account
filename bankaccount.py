104996131925a1603455385e8132b68ae8ee9dc2
Tay <tmitsumori96@gmail.com>
2021-10-27 17:18:55 -0500

    deposit created

--- bankaccount.py
+++ bankaccount.py
@@ -1,8 +1,13 @@
 from random import randint 
 bank_accounts[]
 
-def BankAccount():
+class BankAccount():
     def __init__:(self, full_name, account_number, balance =0):
     self.full_name = full_name
     self.accounts_number = account_number 
     self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"Amount Deposited: ${amount}")
+        print(f"New Balance: ${self.balance}\n")
