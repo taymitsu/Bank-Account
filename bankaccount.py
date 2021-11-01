ed343fbc3ab5c04beee362234cdf935e2bce9a10
Tay <tmitsumori96@gmail.com>
2021-10-31 17:49:49 -0700

    interst and get balanced added

--- bankaccount.py
+++ bankaccount.py
@@ -11,3 +11,23 @@ class BankAccount():
         self.balance += amount
         print(f"Amount Deposited: ${amount}")
         print(f"New Balance: ${self.balance}\n")
+
+    def withdrawl(self, amount):
+        self.balance -= amount 
+        if self.balance < 0:
+            print("INSUFFICIENT FUNDS")
+            print("Overdraft fee of $10 has been applied to your account. \n")
+            self.balance -= 10
+            print(f"Amount withdrawn: ${self.balance}\n")
+        else: 
+            print(f"Amount withdrawn: ${amount}")
+            print(f"New balance: ${self.balance}")
+
+    def get_balance(self):
+        print(f"Account balance: ${self.balance}\n")
+        return self.balance
+
+    def add_interst(self):
+        self.balance += self.balance * 0.00083
+
+    
